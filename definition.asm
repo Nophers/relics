58e3d1a7209d6082fa053288eb3e835264ba85ee
Konrad570 <Konradtoesch@icloud.com>
2022-01-19 11:00:19 +0100

    init

---
+++ definition.asm
@@ -0,0 +1,200 @@
+Sakramente sind sichtbare Zeichen für unsichtbare Gnade, die durch Jesus Christmas und die Kirche vermittelt werden.
+Sakramente sind äußere Zeichen, die mit deutenden Worten ein unsichtbares Geheimnis Gottes versinnbildlicht und darstellt.
+Sakramente sind äußere Zeichen die Gottes Wirken in unserem Leben sichtbar machen wollen, ursprung Jesus Christus.
+
+Sakramente:
+- Taufe
+- Firmung
+- Eucharistie
+- Busse
+- Krankensalbung
+- Ehe 
+- Prieste
+
+Die Anzahl varrierte von 2 (Taufe und Eucharistie) bis zu 30, die heuten eingeschlossen!
+Sieben Zahl wurde auf den Konzil von Trient festgelegt JAHR 1543 - 1565.
+
+--------------------------------------------------------------------------------
+
+Aufgliederung der Sakramente:
+- Geburt (wiedergeborenwerden in Gott)
+- Reife (selbständig werden)
+- Schuld und ihre Bewältigung 
+- Krankheit und Heilung 
+- Liebe und Zeugung
+- Ordnung in der Gemeinschaft der Christen
+
+--------------------------------------------------------------------------------
+
+Spender der einzelnen Sakramente:
+- Taufe: Priester. Diakon, jeder Mensch (bei Nottaufe)
+- Firmung: Bischof, oder Priester
+- Eucharistie: Priester
+- Krankensalbung: Priester
+- Ehe: Ehegotten
+- Priesterweihe: Bischof
+
+Häufigkeit:
+- Taufe: 1x 
+- Firmung: 1x
+- Eucharistie: mehrmals
+- Buße: mehrmals
+- Krankensalbung: mehrmals
+- Ehe: mehrmals
+- Priesterweihe: 1x
+
+--------------------------------------------------------------------------------
+Sakramentalion:
+
+Sind heilige Handlungen, 
+fürbittende Gebete der Kirche für bestimmte Aliegen (Weihwasserweihe, Haussegnung, Autosegnung, Adventkranzweihe, etc.)
+
+Sinn der Sakramentalion:
+Das ganze Leben soll unter dem besonderen Schutz Gottes stehen
+
+Unterschiede Sakramentalion:
+Weihungen: Mensch (z.B. Abt) oder ein Gegenstand (Kirchenglocken) werden für Gott oder für den Gottesdienst bestimmt!
+Segnungen: Der Segen Gottes wird auf Menschen oder Dinge herabgerufen, ohne dass diese dem profanen Gebrauch entzogen werden (Auto)
+
+--------------------------------------------------------------------------------
+
+SAKRAMENTE (Seite 2)
+Latein (1. Jahrhundert): Diensteid, Amtseid -> juristische Bedeutung 
+Griechisch (3. Jahrhundert): "Mysterium" -> Christusereignis, -geheimnis -> "religiöse" Bedeutung
+
+Definition:
+Sakramente sind äußere Zeichen, die eine Wirkung hervorrufen. 
+Der Ursprung der Sakramente liegt in Jesus Christus.
+
+Siebenzahl: 
+Die Anzahl variiert zwischen 2 (Taufe und Abendmahl) bis zu 30.
+Die Siebenzahl wurde endgültig im Konzil von Trient festgelegt. JAHR (1543 - 1565)
+
+Symbolisch 3 + 4 = 7 : Zahl für Vollständigkeit
+
+--------------------------------------------------------------------------------
+
+Ursakrament = Jesus Christus
+in Jesus ist der unsichtbare Gott sichtbar erschienen
+
+Aufgliederung der Sakramente - Entscheidungssituation im Leben
+- Geburt (Taufe)
+- Reife
+- Nahrung zur Erhaltung des Lebens
+- Schuld und ihre Bewältigung
+- Krankheit und Heilung
+- Liebe und Zeugung
+- Ordnung in der Gemeinschaft der Christen 
+
+--------------------------------------------------------------------------------
+
+Sakramente im Überblick:
+
+Sakrament        Spender             Empfänger Häufigkeit 
+
+Taufe          | Prister            | Jeder Mensch 1x                                   
+Busse          | Prister            | Jeder Getaufte                                    
+Eucharistie    | Priester           | Jeder Getaufte im Stande der Gnade                
+Firmung        | Bischof/Priester   | Jeder Getaufter 1x                                      
+Weihe          | Bischof            | Getaufter Gefirmter Mann                          
+Ehe            | Ehepartner         | Jedes Getaufter Christ 1x                         
+Krankensalbung | Prister            | Jeder Schwerkranke sooft er schwer krank ist  
+
+Sinn:
+Sakrament
+
+Taufe
+Aufnahme in die Kirche
+Absage an das Böse 
+Vergebung der Sünden
+Befreiung vom Zwang der Erbschuld.
+
+Busse
+Versöhnung mit Gott, Kirche und den Mitmenschen, Umkehr als Neubeginn
+
+Eucharistie
+Verwirklichung der Kirche als Gemeinschaft Jesu 
+Sein Leben, Sterben und Auferstehen wird in der Feier des Mahles gegenwärtig und erfahrbar
+
+Firmung
+Verantwortung für die kirchliche Gemeinschaft, Vollendung der Taufe
+
+Weihe
+Verbindende Mitte der Gemeinde - in Fortführung der Sendung des
+Zwölferkreises
+Verantwortung für die rechte Entwicklung der Kirche
+
+Ehe
+Abbild der unverlierbaren Treue Gottes, Zeichen der Liebe Christi zur Kirche.
+Kirche im kleinen Hauskirche (ecclesiola)
+
+
+Krankensalbung
+Gott will dem Menschen im in Leid und Krankheit durch die Gemeinschaft
+der Kirche nahe sein und helfen
+
+----
+Zeichen:
+
+Taufe
+Übergießen mit Wasser
+
+Busse
+Lossprechung "Ich spreche dich los von deinen Sünden"
+
+Eucharistie
+Mahl als Symbol der Gemeinschaft !!! Brot, Wein, Einsetzungsbericht (- Wandlung)
+
+Firmung
+Handauflegung - Salbung "Sei besiegelt durch die Gabe Gottes, den heiligen Geist"
+
+Weihe
+Salbung, Handauflegung, Weihegebet
+
+Ehe
+gelebtes Ja-Wort
+
+Krankensalbung
+Salbung, Wort: "Durch diese heilige Salbung hefle dir der Herr in seinem reichen Erbarmen"
+
+
+Ursprung:
+
+Taufe
+Taufbefehl des Auferstandenen
+
+Busse
+Binde und Lösegewalt
+
+Eucharistie
+Letztes Abendmal "Tut dies zu meinem Gedächtnis"
+
+Firmung
+Pfingsfest - Apostel legen die Hände auf - Geistgabe
+
+Weihe
+Handauflegung und Beauftragung durch Apostel
+
+Ehe
+"Ehe sollen im Herrn geschlossen werden"
+
+Krankensalbung
+Salbung, Wort: "Durch diese heilige Salbung hefle dir der Herr in seinem reichen Erbarmen"
+
+
+--------------------------------------------------------------------------------
+
+
+Die Taufe:
+
+Was ist die Taufe? Taufe Bedeutung Taufe Erklärung
+Durch die Taufe wird das Kind in die Gemeinschaft der Kirche aufgenommen.
+Bei der Taufe wird das Kind bei seinem Namen gerufen.
+Die Kirche zeigt, "Du gehörst zu Gott, er ist bei dir"
+
+Zeichen:
+
+- Kreuzzeichen
+Der Priester, die Eltern und die Paten zeichnen dem Täufling ein Kreuz auf die Stirn.
+Ist Erkennungszeichen der Christen
+
